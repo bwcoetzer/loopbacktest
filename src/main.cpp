9476d4b5554bbdf74a8046b64688b2b37bc90917
bwcoetzer <bwcoetzer@gmail.com>
2022-11-04 13:57:04 +0200

    Loopback working on ESP32.  While the loop is made the characters flow.  When the loop is removed the flow stops

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,33 @@
 #define RXD2 16
 #define TXD2 17
 
-void setup() {
+void setup()
+{
   // Note the format for setting a serial port is as follows: Serial2.begin(baud-rate, protocol, RX pin, TX pin);
-  Serial.begin(115200);
-  //Serial1.begin(9600, SERIAL_8N1, RXD2, TXD2);
+  Serial.begin(9600);
+  // Serial.begin(115200);
+
+  while (!Serial)
+  {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+  Serial.println("Goodnight moon!");
+
+  // Serial1.begin(9600, SERIAL_8N1, RXD2, TXD2);
   Serial2.begin(115200, SERIAL_8N1, RXD2, TXD2);
   Serial2.println("serial2test");
   Serial.println("Serial Txd is on pin: " + String(TX));
   Serial.println("Serial Rxd is on pin: " + String(RX));
-
 }
 
-void loop() {
-  Serial2.print("hello");
+void loop()
+{
+  Serial2.print("######");
   Serial2.write("hellowrite");
-  Serial.print(char(Serial2.read()));
-  delay(1000);
+  //Serial.print(Serial2.available());
+  if (Serial2.available() > 0)
+  {
+    Serial.print(char(Serial2.read()));
+  }
+  //delay(200);
 }
